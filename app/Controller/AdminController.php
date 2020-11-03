41b09cf71d42a62e04c1e14ea9323a278f10c790
Vanessa Lopes <vanessa.lopes051@gmail.com>
2020-11-03 19:34:06 -0300

    Alterações aula 12

--- app/Controller/AdminController.php
+++ app/Controller/AdminController.php
@@ -8,7 +8,7 @@ class AdminController{
             $template = $twig->load('admin.html');
 
             $objPostagens = Postagem::selecionaTodos();
-            
+
           $parametros = array();
           $parametros['postagens'] = $objPostagens;
 
@@ -42,4 +42,35 @@ class AdminController{
         }
        
     }
+
+    //FUNÇÃO QUE SELECIONA OS DADOS PARA ALTERAÇÃO
+    public function change($paramId){
+        $loader = new \Twig\Loader\FilesystemLoader('app/View');
+        $twig = new \Twig\Environment($loader);
+        $template = $twig->load('update.html');
+
+        $post = Postagem::selecionaPorId($paramId);
+
+          $parametros = array();
+          $parametros['id'] = $post->id;
+          $parametros['titulo'] = $post->titulo;
+          $parametros['conteudo'] = $post->conteudo;
+
+          $conteudo = $template->render($parametros);
+          echo $conteudo;
     }
+    //FUNÇÃO QUE ALTERA OS DADOS SELECIONADOS
+    public function update(){
+        try{
+            Postagem::update($_POST);
+
+            echo '<script>alert("Publicação alterada com sucesso");</script>';
+            echo'<script>location.href="http://localhost/site-mvc/?pagina=admin&metodo=index"</script>';
+        }catch(Exception $e){
+                echo '<script>alert("'.$e->getMessage().'");</script>';
+                echo'<script>location.href="http://localhost/site-mvc/?pagina=admin&metodo=change&id='.$_POST['id'].'"</script>';
+            }
+        }
+       
+    }
+    
